b0e34dda8853196ab210396f2cacc331a36152da
Kim-ULL <belbet01@kookmin.ac.kr>
2023-11-06 15:29:16 +0900

    Modified: changed memory file - model train

--- settings/changedMemory.py
+++ settings/changedMemory.py
@@ -1,6 +1,36 @@
-count = 5
-for index in range(0, count):
-    cnt = 0
-    for i in range(0,1000000000):
-        cnt += i
-    print(f"{index+1}번째 count 값 : {cnt}")
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+predictions = model(x_train[:1]).numpy()
+predictions
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
