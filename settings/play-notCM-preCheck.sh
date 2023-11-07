733b8f2200b38b693fc05a4693c3779fe5362554
Kim-ULL <belbet01@kookmin.ac.kr>
2023-11-07 17:49:17 +0900

    Modified: changed setting file

--- settings/play-notCM-preCheck.sh
+++ settings/play-notCM-preCheck.sh
@@ -7,20 +7,6 @@ sudo podman ps
 # 현재 시간을 초로 변환
 start_time=$(date +%s)
 
-# 30초 후까지 대기
-while true; do
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-    if [ "$elapsed_time" -ge 30 ]; then
-        break
-    fi
-    sleep 1
-done
-
-# 'pre-checkpoint' 명령어 실행
-sudo podman container checkpoint --pre-checkpoint notChangedMemory
-#sudo podman container checkpoint --print-stats notChangedMemory
-
 # 60초 후까지 대기
 while true; do
     current_time=$(date +%s)
@@ -32,5 +18,18 @@ while true; do
 done
 
 # 'pre-checkpoint' 명령어 실행
-sudo podman container checkpoint --pre-checkpoint notChangedMemory
-#sudo podman container checkpoint --print-stats notChangedMemory
+sudo podman container checkpoint --print-stats --pre-checkpoint notChangedMemory
+
+# 120초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 120 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --print-stats --pre-checkpoint notChangedMemory
+sudo podman stop notChangedMemory
--- settings/play-os-preCheck.sh
+++ settings/play-os-preCheck.sh
@@ -7,20 +7,6 @@ sudo podman ps
 # 현재 시간을 초로 변환
 start_time=$(date +%s)
 
-# 30초 후까지 대기
-while true; do
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-    if [ "$elapsed_time" -ge 30 ]; then
-        break
-    fi
-    sleep 1
-done
-
-# 'pre-checkpoint' 명령어 실행
-sudo podman container checkpoint --pre-checkpoint osImage
-#sudo podman container checkpoint --print-stats osImage
-
 # 60초 후까지 대기
 while true; do
     current_time=$(date +%s)
@@ -32,5 +18,17 @@ while true; do
 done
 
 # 'pre-checkpoint' 명령어 실행
-sudo podman container checkpoint --pre-checkpoint osImage
-#sudo podman container checkpoint --print-stats osImage
+sudo podman container checkpoint --print-stats --pre-checkpoint osImage
+
+# 120초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 120 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --print-stats --pre-checkpoint osImage
