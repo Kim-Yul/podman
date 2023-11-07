f01b8b88c2d07b4bbbacf1697308ada5799e44cf
Kim-ULL <belbet01@kookmin.ac.kr>
2023-11-07 15:05:33 +0900

    Modified: os image bash file

---
+++ settings/play-os-preCheck.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 컨테이너 실행
+sudo podman run -dt --name osImage -it ubuntu
+sudo podman ps
+
+# 현재 시간을 초로 변환
+start_time=$(date +%s)
+
+# 30초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 30 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --pre-checkpoint osImage
+#sudo podman container checkpoint --print-stats osImage
+
+# 60초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 60 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --pre-checkpoint osImage
+#sudo podman container checkpoint --print-stats osImage
