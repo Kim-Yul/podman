df67893b88a131ac45693be34319aad2c412df22
Kim-ULL <belbet01@kookmin.ac.kr>
2023-11-07 18:23:41 +0900

    Modified: file name changing

---
+++ checkpoint-time/notChangedMemory.py
@@ -0,0 +1,4 @@
+count = 31415926535
+print(f"현재 count 값 : {count}")
+context = input("")
+print(f"현재 입력 값 : {context}")
---
+++ checkpoint-time/play-os-preCheck.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 컨테이너 실행
+sudo podman run -dt --name osImage -it ubuntu
+sudo podman ps
+
+# 현재 시간을 초로 변환
+start_time=$(date +%s)
+
+# 60초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 60 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --print-stats --pre-checkpoint osImage
+
+# 120초 후까지 대기
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge 120 ]; then
+        break
+    fi
+    sleep 1
+done
+
+# 'pre-checkpoint' 명령어 실행
+sudo podman container checkpoint --print-stats --pre-checkpoint osImage
+sudo podman stop osImage
+
+# 실험 종료 후 컨테이너 삭제
+sudo podman rm osImage
