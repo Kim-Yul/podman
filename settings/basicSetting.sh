1fd060479c1103231bf81bdf287fac0067205170
Kim-ULL <belbet01@kookmin.ac.kr>
2023-11-07 14:59:16 +0900

    Modified: bash setting changing

--- settings/basicSetting.sh
+++ settings/basicSetting.sh
@@ -3,7 +3,7 @@
 # red hat 에서 podman 을 설치한다.
 if ! rpm -q podman &> /dev/null
 then
-    echo -e "\npodman이 설치되어 있지 않습니다. podman을 설치합니다."
+    echo -e "\npodman이 설치되어 있지 않습니다. podman을 설치합니다.\n"
     sudo dnf -y install podman
 else
     echo -e "\npodman이 이미 설치되어 있습니다."
